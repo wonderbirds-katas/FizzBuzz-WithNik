b493dd672fca00efc5b43e61eab87cd99555a80b
Stefan Boos <kontakt@boos.systems>
2023-08-25 22:09:03 +0200

    .

--- FizzBuzz.Test/FizzBuzzTest.cs
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -13,14 +13,40 @@ public class FizzBuzzTest
         pupil.Answer();
         pupil.Answer().Should().Be(4);
     }
+
+    [Fact]
+    public void Test4()
+    {
+        var teacher = new Teacher(new Pupil());
+        teacher.WantsAnswers(1);
+    }
 }
 
-public class Pupil
+public class Teacher
+{
+    private readonly IPupil _pupil;
+
+    public Teacher(IPupil pupil)
+    {
+        _pupil = pupil;
+    }
+
+    public void WantsAnswers(int numberOfAnswers)
+    {
+        // _pupil.Answer();
+    }
+}
+
+public interface IPupil
+{
+    int Answer();
+}
+
+public class Pupil : IPupil
 {
     private int _count = 1;
     public int Answer()
     {
-        
         return _count ++;
     }
 }
